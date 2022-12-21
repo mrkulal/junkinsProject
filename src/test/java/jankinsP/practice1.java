0625448c7d3e5972020055ecefde2876319517a4
manju <manjunathakr45@gmail.com>
2022-12-21 10:45:05 +0530

    my jjj

---
+++ src/test/java/jankinsP/practice1.java
@@ -0,0 +1,20 @@
+package jankinsP;
+
+import org.testng.annotations.Test;
+
+public class practice1 {
+	@Test(groups="smoke")
+	public void shivu() {
+	int x=10;
+	int y=20;
+	int temp=x;
+	x=y;
+	y=temp;
+	System.out.println(x+" value of x");
+	System.out.println(y+" value of y");
+
+	}
+
+}
+
+
